0b853b8ea2c11e8788dec19cb21d69555cf7c16f
AlexOde <68616269+AlexOd4@users.noreply.github.com>
2026-01-11 22:22:47 +0100

    Refactor player detection from RayCast2D to Area2D
    Replaced RayCast2D nodes with Area2D nodes for near and far detection in the player scene and script. Updated movement logic to use Area2D's position and overlap checks instead of raycasting. Adjusted test scene and project settings to accommodate these changes and ensure compatibility.

--- scripts/gd/player_behaviour.gd
+++ scripts/gd/player_behaviour.gd
@@ -1,27 +1,29 @@
 class_name Player extends CharacterBody2D
 
-@export var movement_distance: Vector2 = Vector2.ONE
+@export_custom(PropertyHint.PROPERTY_HINT_LINK, "") var movement_distance: Vector2 = Vector2.ONE
 @export var movement_turn: int = 1:
 	set(value):
 		movement_turn = clamp(value, 1, INF)
 
-@onready var movement_size: Vector2 = $Sprite2D.scale
+@onready var near_area: Area2D = $NearArea
+@onready var far_area: Area2D = $FarArea
 
-@onready var near_ray: RayCast2D = $NearRay
-@onready var far_ray: RayCast2D = $FarRay
 
 func _ready() -> void:
 	Global.movement_input.connect(_on_movement_input)
-	near_ray.target_position = movement_size * movement_distance
-	far_ray.target_position = movement_size * 2 * movement_distance
+	near_area.position = movement_distance
+	far_area.position = 2 * movement_distance
 func _on_movement_input(global_movement_counter: int, movement_direction: Vector2) -> void:
 	if not global_movement_counter % movement_turn == 0: return
 	
-	near_ray.target_position = movement_size * movement_direction * movement_distance
-	far_ray.target_position = movement_size * 2 * movement_direction * movement_distance
-	#print(global_position-(Vector2(Vector2i(global_position) + movement_direction * movement_size * movement_distance)))
+	var target_positon: Vector2 = movement_direction * movement_distance
+	
+	near_area.position = target_positon
+	far_area.position = 2 * target_positon
+	
 	call_deferred("movement_and_comprovements", movement_direction)
 
 func movement_and_comprovements(movement_direction:Vector2):
-	if near_ray.is_colliding(): return
-	global_position = Vector2(global_position + movement_direction * movement_size * movement_distance)
+	await get_tree().create_timer(0.05).timeout
+	if near_area.has_overlapping_bodies(): return
+	global_position = Vector2(global_position + movement_direction * movement_distance)
