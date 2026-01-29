b2439b42b036be3f38884eca40c8e3f95cce70dc
AlexOde <68616269+AlexOd4@users.noreply.github.com>
2026-01-29 18:47:49 +0100

    level 08 done

--- scripts/gd/moving_area_2d.gd
+++ scripts/gd/moving_area_2d.gd
@@ -1,6 +1,7 @@
 class_name MovingArea2D extends MovableArea2D
 
 @export var base_directions: Array[Vector2] = [Vector2.DOWN]
+@export var do_extra_comprobations: bool
 @onready var base_direction: Vector2 = base_directions[0]
 
 var index: int:
@@ -20,7 +21,6 @@ func _on_movement_input(global_movement_counter: int, _movement_direction:Vector
 	
 	if near_area.has_overlapping_bodies() or (near_area.has_overlapping_areas() and near_area.get_overlapping_areas()[0] is BoxArea2D):
 		for near_areas in near_area.get_overlapping_areas():
-			print(near_areas.name)
 			if near_areas.name == "NearArea": 
 				return
 		
@@ -29,6 +29,17 @@ func _on_movement_input(global_movement_counter: int, _movement_direction:Vector
 		else: next_direction()
 			
 		near_area.position = base_direction * movement_distance
+		#it works haha (its a shit)
+		if do_extra_comprobations:
+			await get_tree().create_timer(0.05).timeout
+			
+			if base_directions.size() > 1:
+				for _index in base_directions.size():
+					if near_area.has_overlapping_bodies() or (near_area.has_overlapping_areas() and near_area.get_overlapping_areas()[0] is BoxArea2D):
+						next_direction()
+						near_area.position = base_direction * movement_distance
+						await get_tree().create_timer(0.05).timeout
+
 	move(base_direction)
 
 func next_direction() -> void:
